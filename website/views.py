3bfe6b9e1330cab0286f9f719b6fabc79a449217
Jordan Bailey <jbailey2@csuchico.edu>
2026-02-24 16:33:36 -0800

    Updated urls/views.py to reflect new blacklist.html page

--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/home.html')
+
+
+def blacklist(request):
+    return render(request, 'website/blacklist.html')
