c89ad1ecbe068dbd626a411d89a48014a7e1e2b8
ImagenL <ilarson3@csuchico.edu>
2026-03-02 13:15:55 -0800

    added nav bar and the functionality

--- website/views.py
+++ website/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/home.html')
+
+def apps(request):
+    return render(request, 'website/apps.html')
+
+def trends(request):
+    return render(request, 'website/trends.html')
+
+def blacklist(request):
+    return render(request, 'website/blacklist.html')
+
+def resources(request):
+    return render(request, 'website/resources.html')
